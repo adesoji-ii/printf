e9fa90a25ca065b1829f74fe6b83398949ef9a42
Amusan Olabisi <138717993+Amusanolabisi@users.noreply.github.com>
2023-09-19 20:46:28 +0100

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * get_precision - calculates the precision for printing
+ * @format: formatted string in which to print the arguments
+ * @i: list of arguments to be printed.
  * @list: list of arguments.
  *
  * Return: Precision.
