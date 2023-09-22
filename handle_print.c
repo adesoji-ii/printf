ccaad3f339632c86f5ddc60e917735154bc245ec
Oluwapelumi Ojo <67923042+adesoji-ii@users.noreply.github.com>
2023-09-22 22:38:09 +0100

    changes

--- handle_print.c
+++ handle_print.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * handle_print - Prints an argument based on its type
+ * handle_print - a function that prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
@@ -11,10 +12,11 @@
  * @size: Size specifier
  * Return: 1 or 2;
  */
+
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknow_len = 0, printed_chars = -1;
+	int i, unknown_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -30,9 +32,9 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-		unknow_len += write(1, "%%", 1);
+		unknown_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
-			unknow_len += write(1, " ", 1);
+			unknown_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
@@ -42,9 +44,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 				--(*ind);
 			return (1);
 		}
-		unknow_len += write(1, &fmt[*ind], 1);
-		return (unknow_len);
+		unknown_len += write(1, &fmt[*ind], 1);
+		return (unknown_len);
 	}
 	return (printed_chars);
 }
-
